e51310375df806c9e81a73dfef7c285dfedae3ce
Irfan Ahmed <irfaniahmed4@gmail.com>
2018-03-21 22:08:56 -0700

    finished what i could

---
+++ clicky-game/src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+import Game from "./components/Game";
+const App = () => <Game />;
+
+
+export default App;
---
+++ clicky-game/src/components/game.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import Nav from "../Nav";
+import Header from "../Header";
+
+
+class Game extends Component {
+  state = {
+    data,
+    score: 0,
+ 
+  };
